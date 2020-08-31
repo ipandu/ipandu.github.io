824130bae0723f2c94ee722e1d269f39c9fd7d65
Ilham Pandu Yuwono <ilhampanduy@gmail.com>
2020-08-31 15:12:10 +0700

    Merperbaiki isi konten, media query, dan menambahkan tooltip

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+//* NAVBAR
 const menuToggle = document.querySelector(".nav-toggle");
 const menuToggleLine = document.querySelectorAll(".nav-toggle .line");
 const navItems = document.querySelector('.nav-items');
 const navItemsLink =  document.querySelectorAll('.nav-items .nav-link');
-
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('open');
@@ -14,3 +14,19 @@ menuToggle.addEventListener('click', () => {
         link.classList.toggle('open');
     })
 })
+
+//* Top Button
+const topBtn = document.querySelector('.top-btn');
+window.onscroll = function() {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        console.log('1');
+        topBtn.style.display = "flex";
+    } else {
+        console.log('0');
+        topBtn.style.display = "none";
+    }
+}
